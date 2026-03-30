66c9a4b7000e0636a67ff850272c1921bbdffd3a
daniel <machadodaniel281@gmail.com>
2026-03-30 17:23:14 -0600

    server.js completo y corregido

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
-// Start server
+// Puerto obligatorio de Railway
 const PORT = process.env.PORT || 3000;
+
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
